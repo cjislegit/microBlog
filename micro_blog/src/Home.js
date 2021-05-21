d8f174059bc0be158db6e305026466d2c0830e4e
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-05-21 16:14:02 -0700

    Moved blg data to its own file in the data folder

--- micro_blog/src/Home.js
+++ micro_blog/src/Home.js
@@ -1,26 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
+  const [name, setName] = useState('Mia');
+
+  const handleNameChange = () => {
+    setName('Goose');
   };
+
+  // Runs on every render
+  // useEffect(() => {
+  //   console.log('Use effect ran');
+  // });
+
+  // Runs on first render
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  // Runs on first render
+  useEffect(() => {
+    console.log('First render or name change.');
+  }, [name]);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} />}
+      <button onClick={handleNameChange}>Change Name</button>
+      <p>{name}</p>
     </div>
   );
 };
