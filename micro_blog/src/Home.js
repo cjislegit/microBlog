4d101adac93824096875ccc4ce855dda7f0ed2ca
Carlos Ramirez <carlosjramirez11@gmail.com>
2021-05-22 13:51:15 -0700

    Error data is now displayed in a div

--- micro_blog/src/Home.js
+++ micro_blog/src/Home.js
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // const [name, setName] = useState('Mia');
 
@@ -28,9 +29,11 @@ const Home = () => {
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null);
       })
       .catch((err) => {
-        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
       });
   }, []);
 
@@ -41,6 +44,7 @@ const Home = () => {
 
   return (
     <div className='home'>
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} />}
       {/* <button onClick={handleNameChange}>Change Name</button>
